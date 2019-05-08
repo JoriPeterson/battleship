44d27c3ee18f7541c6d957d8c189577f358ee45a
Sarah Tatro <statrox4@gmail.com>
2019-05-08 13:50:16 -0600

    Add render method

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -41,9 +41,36 @@ class CellTest < MiniTest::Test
     @cell.fire_upon
     assert_equal @cell.ship.health, 2
     assert @cell.fired_upon?
-    #
-    # cruiser.hit
-    # cruiser.sunk?
   end
+
+  def test_renders_returns_empty_or_miss
+    @cell.render
+    # "." = ship not fired upon
+    assert_equal ".", @cell.render
+    @cell.fire_upon
+    @cell.render
+    # "M" = ship fired upon & missed
+    assert_equal "M", @cell.render
+  end
+
+  def test_ship_two_renders_sunk_and_hit
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2 = Cell.new("B4")
+
+    cell_2.place_ship(cruiser)
+    assert_equal ".", cell_2.render
+    # "S" = reveal ship placement but it has not been fired upon
+    assert_equal "S", cell_2.render(true)
+    # cell_2.fire_upon
+    # # "H" = ship hit
+    # assert_equal "H", cell_2.render
+    # refute cruiser.sunk?
+    # cruiser.hit
+    # cruiser.hit
+    # assert cruiser.sunk?
+    # # "X" = ship has sunk
+    # assert_equal "X", cell_2.render
+  end
+
 
 end
