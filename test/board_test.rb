202e3d7c698294bccda656ffe7bea760b176d8d5
Jori Peterson <joripeterson@gmail.com>
2019-05-09 10:26:08 -0600

    Add cell hash testing method

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,8 +7,17 @@ require 'pry'
 
 class BoardTest < MiniTest::Test
 
-  def test_it_exists
-    board = Board.new
+  def setup
+    @board = Board.new
   end
+
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+  def test_cells_return_coordinates_and_cell_objects
+    assert_equal 16, @board.cells.length
+  end
+
 
 end
