0d3b871c1b26de44f45847b0eb21ac8813914f48
Jori Peterson <joripeterson@gmail.com>
2019-05-09 10:24:54 -0600

    Initialize cells and coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -1,47 +1,27 @@
+require './lib/cell'
+
 class Board
+  attr_reader :cells
 
   def initialize
-  end
-
-  def cells
-    cells = {
-     "A1" => @cell_1,
-     "A2" => @cell_2,
-     "A3" => @cell_3,
-     "A4" => @cell_4,
-     "B1" => @cell_5,
-     "B2" => @cell_6,
-     "B3" => @cell_7,
-     "B4" => @cell_8,
-     "C1" => @cell_9,
-     "C2" => @cell_10,
-     "C3" => @cell_11,
-     "C4" => @cell_12,
-     "D1" => @cell_13,
-     "D2" => @cell_14,
-     "D3" => @cell_15,
-     "D4" => @cell_16
+    @cells = {
+     "A1" => Cell.new("A1"),
+     "A2" => Cell.new("A2"),
+     "A3" => Cell.new("A3"),
+     "A4" => Cell.new("A4"),
+     "B1" => Cell.new("B1"),
+     "B2" => Cell.new("B2"),
+     "B3" => Cell.new("B3"),
+     "B4" => Cell.new("B4"),
+     "C1" => Cell.new("C1"),
+     "C2" => Cell.new("C2"),
+     "C3" => Cell.new("C3"),
+     "C4" => Cell.new("C4"),
+     "D1" => Cell.new("D1"),
+     "D2" => Cell.new("D2"),
+     "D3" => Cell.new("D3"),
+     "D4" => Cell.new("D4")
      }
-   end
-end
-
-class Cell < Board
-
-  @cell_1 = Cell.new
-  @cell_2 = Cell.new
-  @cell_3 = Cell.new
-  @cell_4 = Cell.new
-  @cell_5 = Cell.new
-  @cell_6 = Cell.new
-  @cell_7 = Cell.new
-  @cell_8 = Cell.new
-  @cell_9 = Cell.new
-  @cell_10 = Cell.new
-  @cell_11 = Cell.new
-  @cell_12 = Cell.new
-  @cell_13 = Cell.new
-  @cell_14 = Cell.new
-  @cell_15 = Cell.new
-  @cell_16 = Cell.new
+  end
 
 end
