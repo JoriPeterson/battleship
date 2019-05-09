144a4ab3ac8623bfe192769f22844e20d222a3ee
Jori Peterson <joripeterson@gmail.com>
2019-05-08 20:08:24 -0600

    Start board test file

---
+++ test/board_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require 'pry'
+
+class BoardTest < MiniTest::Test
+
+  def test_it_exists
+    board = Board.new
+  end
+
+end
